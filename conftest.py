83c08736f4b45d4911682908e266ab8fe241e1f2
Martin Maiksnar <majxik@gmail.com>
2024-07-05 00:40:32 +0200

    Dry code from test file to conftest

--- conftest.py
+++ conftest.py
@@ -1,6 +1,7 @@
 import pytest
 from playwright.sync_api import sync_playwright
 from calculator_page import CalculatorPage
+from query_page import QueryPage
 
 #Author: Martin Maiksnar
 #This is a fixture for the calculator page
@@ -14,4 +15,14 @@ def calc_page():
         calc_page = CalculatorPage(page)
         calc_page.open()
         yield calc_page  
+        browser.close()
+
+@pytest.fixture
+def query_page():
+    with sync_playwright() as p:
+        browser = p.chromium.launch()
+        page = browser.new_page()
+        query_page = QueryPage(page)
+        query_page.navigate()
+        yield query_page
         browser.close()
