b37e42c134f583f16b057c4f1740c007e1d5cfd5
Martin Maiksnar <majxik@gmail.com>
2024-07-06 12:54:06 +0200

    Dry out fixture methodes for calc_page and query_page to browser_context methode

--- conftest.py
+++ conftest.py
@@ -4,25 +4,31 @@ from calculator_page import CalculatorPage
 from query_page import QueryPage
 
 #Author: Martin Maiksnar
-#This is a fixture for the calculator page
+#This is a fixture for the calculator page & query page
 
-@pytest.fixture
-def calc_page():
-    with sync_playwright() as p:
-        # For debuging, you can set headless=False, slow_mo=100
-        browser = p.chromium.launch()
-        page = browser.new_page()
-        calc_page = CalculatorPage(page)
-        calc_page.open()
-        yield calc_page  
-        browser.close()
+import pytest
+from playwright.sync_api import sync_playwright
 
-@pytest.fixture
-def query_page():
+# Generic fixture to manage browser context
+@pytest.fixture(scope="function")
+def browser_context():
     with sync_playwright() as p:
         browser = p.chromium.launch()
-        page = browser.new_page()
-        query_page = QueryPage(page)
-        query_page.navigate()
-        yield query_page
+        yield browser
         browser.close()
+
+# Calc_page fixture using the browser_context fixture
+@pytest.fixture
+def calc_page(browser_context):
+    page = browser_context.new_page()
+    calc_page = CalculatorPage(page)
+    calc_page.open()
+    yield calc_page
+
+# Query_page fixture using the browser_context fixture
+@pytest.fixture
+def query_page(browser_context):
+    page = browser_context.new_page()
+    query_page = QueryPage(page)
+    query_page.navigate()
+    yield query_page
